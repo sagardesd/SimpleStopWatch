bcbfb4d552840b3da2a2a1dcadfa8d78487c4bc8
Sagar Kumar Madala <msagar@TOSHIBA-TSIP.COM>
2018-11-27 18:38:12 +0530

    formated date and time support tests

--- test.cpp
+++ test.cpp
@@ -7,14 +7,18 @@ int main(int argc, char* argv[])
 {
 	int j = 0;
 	//StopWatch stopWatch(SECOND);
-	StopWatch stopWatch(MILISECOND);
-	//StopWatch stopWatch(MICROSECOND);
+	//StopWatch stopWatch(MILISECOND);
+	StopWatch stopWatch(MICROSECOND);
 	stopWatch.start();
 	while(j != 1000)
 	{
 		j++;
 	}
 	double timeElapsed = stopWatch.stop();
+	std::string startTime = stopWatch.getStartDateAndTime();
+	std::string endTime = stopWatch.getStopDateAndTime();
+	std::cout << "Stop watch started at :" << startTime << std::endl;
+	std::cout << "Stop watch stopped at :" << endTime << std::endl;
 	std::cout << "Time elapsed : " << timeElapsed << std::endl;
 	return 0;
 }
